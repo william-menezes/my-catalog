40b038a4df4983e1c9a418b9cb3c21266efa4456
William Menezes Damascena <williamenezes@outlook.com>
2025-05-15 22:47:46 -0300

    :wrench: chore: Installing PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import MyPreset from '../assets/mypreset';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
+  ],
 };
